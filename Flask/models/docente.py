dfafdc9a0151e5c884df03e008d3698f17df9498
Mauricio Guachizaca <149254767+mauricioguachizaca@users.noreply.github.com>
2024-05-14 21:56:33 -0500

    modelos combios estudiantes docentes

--- Flask/models/docente.py
+++ Flask/models/docente.py
@@ -1,12 +1,7 @@
 from Flask.models.persona import Persona
 class Docente(Persona): # Hereda de Persona
-    def __init__(self, id_docente, nombre, apellido, cedula, rol):
-        super().__init__()  # Llama al constructor de la clase base (Persona)
-        self.id = id_docente  # Atributo específico de Docente
-        self.nombre = nombre
-        self.apellido = apellido
-        self.cedula = cedula
-        self.rol = rol
-
+    def __init__(self):
+        super().__init__(d=0,nombre="",apellido="",cedula="",rol=None)  # Llama al constructor de la clase base (Persona)
+    
     def __str__(self):
         return f"Docente: {self.nombre} {self.apellido}"
--- Flask/models/estudiante.py
+++ Flask/models/estudiante.py
@@ -1,12 +1,8 @@
 from Flask.models.persona import Persona
 class Estudiante(Persona): # Hereda de Persona
-    def __init__(self, id_estudiante, nombre, apellido, cedula, rol):
-        super().__init__()  # Llama al constructor de la clase base (Persona)
-        self.id = id_estudiante  # Atributo específico de Estudiante
-        self.nombre = nombre
-        self.apellido = apellido
-        self.cedula = cedula
-        self.rol = rol
-
+    def __init__(self):
+        super().__init__(id=0,nombre="",apellido="",cedula="",rol=None)  # Llama al constructor de la clase base (Persona)
+ 
+ 
     def __str__(self):
-        return f"Estudiante: {self.nombre} {self.apellido}"
+        return f"Estudiante:"
