fbb245948710bc5e16b91751323b4149a2a7db57
Bradley <bradley.poma@unl.edu.ec>
2024-05-21 21:19:59 -0500

    api

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
  
+ 
 def create_app():
  
     app = Flask(__name__, instance_relative_config=False)
--- Flask/routes/api.py
+++ Flask/routes/api.py
@@ -3,13 +3,17 @@ from Flask.funtions.readExel import ReadDocentesExel
 from flask_cors import CORS
 import os
 api = Blueprint('api', __name__)
+
 #get para presentar los datos
 #post para enviar los datos, modificar y iniciar sesion
 
 ALLOWED_EXTENSIONS = {'xlsx'}
-
+ 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
 
 @api.route('/exel_docente', methods=['POST'])
 def upload_file_docente():
