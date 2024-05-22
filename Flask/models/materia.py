18a110d396894a53e1afcfce5efabf1e5c16a673
Bradley <bradley.poma@unl.edu.ec>
2024-05-21 21:18:59 -0500

    commit

--- Flask/models/materia.py
+++ Flask/models/materia.py
@@ -5,45 +5,6 @@ class Materia:
         self.__nombre = ""
         self.__cicloId = ''
 
-    @property
-    def _id(self):
-        return self.__id
-
-    @_id.setter
-    def _id(self, value):
-        self.__id = value
-
-    @property
-    def _nombre(self):
-        return self.__nombre
-
-    @_nombre.setter
-    def _nombre(self, value):
-        self.__nombre = value
-
-    @property
-    def _ciclo(self):
-        return self.__ciclo
-
-    @_ciclo.setter
-    def _ciclo(self, value):
-        self.__ciclo = value
-
-    @property
-    def serializable(self):
-        return {
-            "id": self._id,
-            "nombre": self._nombre,
-            "ciclo": self._ciclo
-        }
     
-    def deserialize(self, data):
-        materia = Materia() 
-        materia._id = data["id"]
-        materia._nombre = data["nombre"]
-        materia._ciclo = data["ciclo"]
-        return materia
-
-
 
     
