821881edbc14cc522138a6d62796b0d35a1867a5
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2024-06-01 14:51:21 -0500

    Validacion correos
    Falta la verificación

--- sistema-re_react/src/pages/login/iniciosesion.jsx
+++ sistema-re_react/src/pages/login/iniciosesion.jsx
@@ -2,11 +2,27 @@ import React, { useEffect } from 'react';
 import 'tailwindcss/tailwind.css';
 import { Link } from 'react-router-dom';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import './style.css'; // Incluimos los estilos adicionales
+import { useFormik } from 'formik';
+
+
+
+
 
 const Iniciosesion = () => {
+
+  const validate = (values) => {
+    const errors = {};
   
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@unl\.edu\.ec$/i.test(values.email)) {
+      errors.email = 'Correo invalido el dominio debe ser con @unl.edu.ec';
+    }
+  
+    return errors
+  }
+
   useEffect(() => {
     const container = document.getElementById('container');
     const registerBtn = document.getElementById('register');
@@ -36,6 +52,17 @@ const Iniciosesion = () => {
     };
   }, []);
 
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="container" id="container">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
@@ -49,8 +76,12 @@ const Iniciosesion = () => {
             <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
           </div>
           <span>Te enviaremos una clave de activación a tu correo</span>
-          <input type="email" placeholder="correo@unl.edu.ec" required />
-          <input type="password" placeholder="Tu codigo de activación" required />
+          <input type="email" placeholder="correo@unl.edu.ec" required name="email" id="email"
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email}/>
+          {formik.touched.email && formik.errors.email ? (
+            <span style={{ color: 'red' }}>{formik.errors.email}</span>
+          ) : null}
+
           <button type="submit">Validar</button>
         </form>
       </div>
@@ -64,12 +95,19 @@ const Iniciosesion = () => {
             <a href="https://www.unl.edu.ec/" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
           </div>
           <span>Ingresa tus credenciales institucionales | UNL</span>
-          <input type="email" placeholder="correo@unl.edu.ec" required />
+          <input type="email" placeholder="correo@unl.edu.ec" required name="email" id="email"
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email}/>
+          {formik.touched.email && formik.errors.email ? (
+            <span style={{ color: 'red' }}>{formik.errors.email}</span>
+          ) : null}
+
+
           <input type="password" placeholder="contraseña" required />
           <a href="#">¿Olvidaste tu contraseña?</a>
           <button type="submit">Iniciar sesión</button>
         </form>
       </div>
+
       <div className="toggle-container">
         <div className="toggle">
           <div className="toggle-panel toggle-left">
@@ -77,6 +115,7 @@ const Iniciosesion = () => {
             <p>Si ya realizaste correctamente tu validación, ya eres parte de nuestro sistema !Bienvenido¡</p>
             <button id="login">Ingresa aquí</button>
           </div>
+
           <div className="toggle-panel toggle-right">
             <h1>FORANIX</h1>
             <p>Si eres nuevo por aquí, debes realizar la activación de tu correo
